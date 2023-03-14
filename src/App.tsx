f338fe4075d7dba7fe39a4187a94fd2dd3f59193
Sergio Henrique <sergionavire@gmail.com>
2023-03-14 10:18:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const nome = 'Sergio';
+  return <div>
+    <h1>Sou um título do {nome}</h1>
+    <p>Sou um paragrafo</p>
+  </div>;
+}
+
+export default App;
